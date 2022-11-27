0866997b845caf991f19f731d91fb16345cdfca4
eeeds <encinaesteban27@gmail.com>
2022-11-27 09:43:55 -0300

    custom-dag is working

--- dags/notifications.py
+++ dags/notifications.py
@@ -1,9 +1,9 @@
 from airflow.models.baseoperator import BaseOperator
 
 class Notifications(BaseOperator):
-    def __init__(self, msg:str,*args, **kwargs):
+    def __init__(self, mensaje:str,*args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.msg = msg
+        self.mensaje = mensaje
     
-    def send_message(self, context):
-        print(f'Esta es una notificacion: {self.msg} {{ds}}')
+    def execute(self, context):
+        print(f'Esta es una notificacion: {self.mensaje}')
